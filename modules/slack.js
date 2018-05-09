61b25dd54dab67a7631d4bd97e3ab8e3c1251d89
Alyson Takai <shtakai@gmail.com>
2018-05-09 21:26:33 +0700

    Accept attachment

--- modules/slack.js
+++ modules/slack.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const config = require('../config')
 
-const sendDM = async (userId, message) => {
+const sendDM = async (userId, message, attachments) => {
   try {
     const conversationResponse = await axios.post(
       'https://slack.com/api/conversations.open',
@@ -20,6 +20,7 @@ const sendDM = async (userId, message) => {
       {
         channel: conversationResponse.data.channel.id,
         text: message,
+        attachments: JSON.stringify(attachments),
       }, {
         headers: {
           'Authorization': `Bearer ${config.botToken}`,
