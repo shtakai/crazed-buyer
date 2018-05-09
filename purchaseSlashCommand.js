d120703e2d341371777c4cb0c834e8a8a7f1b844
Alyson Takai <shtakai@gmail.com>
2018-05-09 09:11:17 +0700

    Add main file

---
+++ purchaseSlashCommand.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/purchase', async (req, res) => {
+  console.log(req.body)
+  res.send('OK')
+})
+
+const PORT = 9999
+app.listen(PORT, () => {
+  console.log(`Slack bot server has started on port ${PORT}`)
+})
